0236f6563fe420496a6db4c95e5e17acb211e118
Huangxx <huangxiaoxin@cmbc.com.cn>
2018-01-30 11:15:04 +0800

    add shellsort

--- src/test/sort/Sort.java
+++ src/test/sort/Sort.java
@@ -17,10 +17,23 @@ public final class Sort {
 		}
 
 	}
+	
+	public static <T extends Comparable<? super T>> void shellSort(T[] array){
+		for(int gap=array.length/2;gap>0;gap/=2){
+			int j;
+			for(int i=gap; i<array.length;i+=gap){
+				T tmp = array[i];
+				for(j=i; j>0 && tmp.compareTo(array[j-gap])<0;j-=gap){
+					array[j] = array[j-gap];
+				}
+				array[j]=tmp;
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		Integer[] testArray = { 3, 1, 5, 0, 9, 88, 77 };
-		insertSort(testArray);
+		shellSort(testArray);
 		System.out.println(Arrays.asList(testArray));
 	}
 
